f60ad55f0e4e923ce3600a2e6472328c47d51cef
Thaís Souto <thaissoutoalves@gmail.com>
2025-04-05 20:21:12 -0300

    content created

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -4,8 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-menu-bar',
   imports: [],
   templateUrl: './menu-bar.component.html',
-  styleUrl: './menu-bar.component.scss'
+  styleUrl: './menu-bar.component.scss' // não consegui colocar o scss responsivo aqui, deu erro './menu-bar.resp.component.scss'
+  
 })
+
+
 export class MenuBarComponent {
 
 }
