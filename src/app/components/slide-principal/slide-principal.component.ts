53d627fbea7e818b56d8dd3766b78fd1164b151d
Thaís Souto <thaissoutoalves@gmail.com>
2025-04-05 19:54:04 -0300

    components created

---
+++ src/app/components/slide-principal/slide-principal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-principal',
+  imports: [],
+  templateUrl: './slide-principal.component.html',
+  styleUrl: './slide-principal.component.scss'
+})
+export class SlidePrincipalComponent {
+
+}
