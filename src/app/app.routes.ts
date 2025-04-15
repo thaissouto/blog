74f9b0ea1b32874f613399675f93115318e60e68
Thaís Souto <thaissouto@MacBook-Air-Thais-Souto.local>
2025-04-15 00:10:24 -0300

    novaversao.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProjetosComponent } from './pages/projetos/projetos.component';
+import { ContatoComponent } from './pages/contato/contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'projetos', component: ProjetosComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: '', redirectTo: '/projetos', pathMatch: 'full' } // opcional: redireciona pra "projetos" ao abrir
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/custom-cursor/custom-cursor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-cursor',
+  imports: [],
+  templateUrl: './custom-cursor.component.html',
+  styleUrl: './custom-cursor.component.css'
+})
+export class CustomCursorComponent {
+
+}
